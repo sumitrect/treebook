6e45a196891c44b99fcc481fa9ff169a36c896a6
Sumit Garg <sumit.rect@gmail.com>
2014-06-21 00:59:17 +0530

    Created a new git application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '624ef3806a237d8a030f58be68af3170feca5c42bb3ae377bcd6ee737f36f298457b1595f9df85a7202889a50987319fb4c9908249747a2055a899b140e9cc57'
